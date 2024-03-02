aa2eda02789a2572b8dd07110d4ed584bdafb464
rachel <rachelricedev@gmail.com>
2024-03-02 14:00:32 -0800

    added database connection

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'store'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
