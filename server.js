7518ebf2fc49672e10a78cab11664f7d2b6e956e
rachel <rachelricedev@gmail.com>
2024-03-06 19:58:02 -0800

    finished setting up server

--- server.js
+++ server.js
@@ -41,6 +41,11 @@ let db,
     //     }
     //   }
 
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
