c87da398619050ae2b54bfdde436fa6e7881342d
rachel <rachelricedev@gmail.com>
2024-03-06 20:01:11 -0800

    added css directory and stylesheet

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ let db,
 
     app.set('view engine', 'ejs')
     app.use(express.static('public'))
-    app.use(express.urlencoded({ extended: true }))
+    app.use(express.urlencoded({ extended: true })) 
     app.use(express.json())
 
 app.listen(process.env.PORT || PORT, ()=>{
