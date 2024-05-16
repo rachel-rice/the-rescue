d68de34566ec115114f6a7c397e2c6a46095cf57
rachel <rachelricedev@gmail.com>
2024-05-15 20:05:20 -0700

    added form for adding rescues

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-// this is a test push
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteRescue)
+})
+
+// Array.from(thumbText).forEach((element)=>{
+//     element.addEventListener('click', addLike)
+// })
+
+async function deleteRescue(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteRescue', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'primaryNameS': pName,
+              'primaryBreedS': pBreed
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
