f74b29bc30f6465bc16defc095497954444dcd50
rachel <rachelricedev@gmail.com>
2024-05-31 18:10:20 -0700

    fixed multiple errors, close to workable, middleware added

--- server.js
+++ server.js
@@ -1,72 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
 
-const connectDB = require('./config/database');
-const mainRoutes = require('./routes/home');
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
 
-require('dotenv').config({path: './config/.env'})
+// Passport config
+require("./config/passport")(passport);
 
-connectDB()
+//Connect To Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI}),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Use flash messages for errors, info, ect...
+app.use(flash());
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
 
 
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended: true })) 
-app.use(express.json())
-
-
-
-
-app.use('/', mainRoutes)
-
-    // app.get('/',(request, response)=>{
-    //     db.collection('rescues').find().sort({likes: -1}).toArray()
-    //     .then(data => {
-    //         response.render('index.ejs', { info: data })
-    //     })
-    //     .catch(error => console.error(error))
-    // })
-
-    // app.post('/addRescue', (request, response) => {
-    //     db.collection('rescues').insertOne({primaryBreed: request.body.primaryBreed,
-    //     primaryName: request.body.primaryName, likes: 0})
-    //     .then(result => {
-    //         console.log('Rescue Added')
-    //         response.redirect('/')
-    //     })
-    //     .catch(error => console.error(error))
-    // })
-
-    // app.put('/addOneLike', (request, response) => {
-    //     db.collection('rescues').updateOne({primaryBreed: request.body.primaryBreedS, primaryName: request.body.primaryNameS,likes: request.body.likesS},{
-    //         $set: {
-    //             likes:request.body.likesS + 1
-    //           }
-    //     },{
-    //         sort: {_id: -1},
-    //         upsert: true
-    //     })
-    //     .then(result => {
-    //         console.log('Added One Like')
-    //         response.json('Like Added')
-    //     })
-    //     .catch(error => console.error(error))
-    
-    // })
-
-    // app.delete('/deleteRescue', (request, response) => {
-    //     db.collection('rescues').deleteOne({primaryBreed: request.body.primaryBreedS})
-    //     .then(result => {
-    //         console.log('Rescue Deleted')
-    //         response.json('Rescue Deleted')
-    //     })
-    //     .catch(error => console.error(error))
-    
-    // })
-    
-    app.listen(process.env.PORT, ()=>{
-        console.log('Server is running, you better catch it!')
-    }) 
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
