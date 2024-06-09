220637ea001a1e3420d38cdb86e3b6832314470f
rachel <rachelricedev@gmail.com>
2024-06-08 19:46:28 -0700

    fixed cb not a function error in user model and passport config

--- models/User.js
+++ models/User.js
@@ -8,7 +8,6 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Password hash middleware.
-
 UserSchema.pre("save", function save(next) {
   const user = this;
   if (!user.isModified("password")) {
@@ -29,13 +28,14 @@ UserSchema.pre("save", function save(next) {
 });
 
 // Helper method for validating user's password.
-
-UserSchema.methods.comparePassword = function comparePassword(
-  candidatePassword,
-  cb
-) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(isMatch);
+    });
   });
 };
 
