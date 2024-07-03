d2b354513ab12d0aa2244ceb3448519bc4f35e86
rachel <rachelricedev@gmail.com>
2024-07-02 21:52:28 -0700

    fixed rescue controller to display add a rescue page

--- controllers/rescue.js
+++ controllers/rescue.js
@@ -1,7 +1,21 @@
 module.exports = {
     getRescue: (req, res) => {
-      res.render('rescue.ejs');
+  const posts = [];
+      res.render('rescue.ejs', {posts: posts });
     }
   };
   
+
+  // const getRescue = (req, res) => {
+  //   // Fetch posts from the database or any other source
+  //   const posts = [
+  //     // { _id: 1, name: 'Post 1' },
+  //     // { _id: 2, name: 'Post 2' },
+  //     // Add more posts as needed
+  //   ];
   
+  //   // Pass the posts to the EJS template
+  //   res.render('rescue', { posts: posts });
+  // };
+  
+  // module.exports = { getRescue };
